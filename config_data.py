cf1e6590036ce8389d14e3822074e336b1496f6d
NicoleMp2 <el18604@mail.ntua.gr>
2024-01-24 14:37:17 +0200

    erwtima 1

--- config_data.py
+++ config_data.py
@@ -5,16 +5,18 @@ from pyspark.sql.types import DateType, IntegerType, DoubleType
 from pyspark.sql.functions import to_date
 
 
-spark = SparkSession.builder.appName("PrepareData").getOrCreate()
-
+# spark = SparkSession.builder.appName("PrepareData").getOrCreate()
+spark = SparkSession.builder.appName("PrepareData")\
+    .config("spark.sql.legacy.timeParserPolicy", "LEGACY")\
+    .getOrCreate()
 crime_data_2010_to_2019 = spark.read.csv("Crime_Data_from_2010_to_2019.csv",header=True, inferSchema=True)
 crime_data_2020_to_present = spark.read.csv("Crime_Data_from_2020_to_Present.csv",header=True, inferSchema=True)
+
 
 crime_data = crime_data_2010_to_2019.union(crime_data_2020_to_present)
 
 # Adjust data types using select and cast
-#Rename columns and change data types
-# crime_data = crime_data.toDF(*['Date Rptd', 'DATE OCC', 'Vict Age', 'LAT', 'LON', 'Category', 'Descript', 'DayOfWeek', 'Time', 'PdDistrict', 'Address', 'Location', 'LOSD District', 'Community District', 'Council District', 'Status', 'Updated On', 'URL'])
+
 crime_data = crime_data.withColumn('Date Rptd', to_date(col('Date Rptd'), 'MM/dd/yyyy').cast(DateType()))
 crime_data = crime_data.withColumn('DATE OCC', to_date(col('DATE OCC'), 'MM/dd/yyyy').cast(DateType()))
 crime_data = crime_data.withColumn('Vict Age', col('Vict Age').cast(IntegerType()))
@@ -24,6 +26,8 @@ print("Total Rows:", crime_data.count())
 crime_data.printSchema()
 
 
-crime_data.coalesce(1).write.format("csv").save("crime_data.csv", header=True)
+# crime_data.coalesce(1).write.format("csv").save("crime_data.csv", header=True)
+# Write the DataFrame to a CSV file
+crime_data.coalesce(1).write.format("csv").mode("overwrite").option("header", True).save("crime_data.csv")
 
 spark.stop()
